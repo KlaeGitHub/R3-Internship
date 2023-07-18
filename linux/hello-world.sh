28e3b36db36d86b20d09717f17b393d0e9cac0e0
KlaeGitHub <klae28@outlook.com>
2023-07-18 14:41:08 +0100

    i asked for the users name and age

--- linux/hello-world.sh
+++ linux/hello-world.sh
@@ -1,1 +1,15 @@
-echo 'hello world!' 
+echo 'whats your name? '
+read varname
+echo its nice to meet you $varname 
+echo 'how old are you`?'
+read varage 
+echo your $varage years old!
+
+echo  your name is $varname 
+echo and your $varage years old.
+echo nice!
+
+  
+
+
+
