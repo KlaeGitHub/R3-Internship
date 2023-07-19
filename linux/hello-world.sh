8f9a93ab7c3871fd19b3ae793c5227627865550c
KlaeGitHub <139763537+KlaeGitHub@users.noreply.github.com>
2023-07-19 11:35:47 +0100

    i asked for the users name and age (#1)
    * i asked for the users name and age
    
    * i removed the unnessacary lines

--- linux/hello-world.sh
+++ linux/hello-world.sh
@@ -1,1 +1,12 @@
-echo 'hello world!' 
+echo 'whats your name? '
+read varname
+echo its nice to meet you $varname 
+echo 'how old are you`?'
+read varage 
+echo your $varage years old!
+
+
+  
+
+
+
