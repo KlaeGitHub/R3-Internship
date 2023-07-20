0df09f1377fb470146a3c94c124c27792d24cac2
KlaeGitHub <klae28@outlook.com>
2023-07-20 17:33:08 +0100

    added python files and screenshot

---
+++ pythonprac/pythoncalc.py
@@ -0,0 +1,7 @@
+first = input("first: ")
+second = input("second: ")
+' allows user to input a number'
+sum = float(first) + float(second)
+'adds first input to second, we need to put <float> so it turns it into a integer which can be decimals aswell. '
+print(sum)
+' prints the answer'
