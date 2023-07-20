90850022fbced3b94193f11b43699a6aa9d517d8
KlaeGitHub <klae28@outlook.com>
2023-07-20 11:42:33 +0100

    i vcreated pythonwork folder

---
+++ pythonprac/pythonprac.py
@@ -0,0 +1,4 @@
+name = input("what is your name? ")
+'this line of code allows the user to input something... in this case, their name'
+print("hello " + name)
+' this line will use the answer to the first line of code and repeat their name back saying... (hello <name-entered> ) ' 
---
+++ pythonprac/pythonprac1.py
@@ -0,0 +1,5 @@
+birth_year = input("enter your birth year: ")
+' this line of code allows the user to input thei birthyear' 
+age = 2023 - int(birth_year)
+' this line of code takes the current year (2023) and subtracts it from the users entered birthyear in order to calculate its age. '
+'The reason we had to put `(int)infront of <birthyear>'
